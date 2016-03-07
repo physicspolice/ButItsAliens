ef746223b164e5191eb63d83d4113a2469aaa4a9
James Weisner <james.weisner@gmail.com>
2016-03-06 22:23:39 -0800

    Initial checkin.

---
+++ compute.py
@@ -0,0 +1,23 @@
+from os.path import join, exists
+from json import loads, dumps
+from csv import reader
+
+with open('sources.json') as file:
+	sources = loads(file.read())
+
+for name, url in sources:
+	path = join('sources', name.lower().replace(' ', '-') + '.csv')
+	if not exists(path):
+		print("Missing source file: %s" % path)
+		continue
+	with open(path) as file:
+		csv = reader(file)
+		csv.next() # Skip header row.
+		points = [row for row in csv]
+		for index, circle in enumerate(sharpshooter(points)):
+			with open(join('circles', str(index) + '.json'), 'w') as file:
+				file.write(dumps(circle))
+
+def sharpshooter(points):
+	# Do the texas sharpshooter thing.
+	return []
