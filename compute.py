648e44d946332a484d5c21f3d784f80829754d95
James Weisner <james.weisner@gmail.com>
2016-03-10 00:53:43 -0800

    cross-track distance
    Working on cross-track distance function.

--- compute.py
+++ compute.py
@@ -1,10 +1,36 @@
 from os.path import join, exists
 from json import loads, dumps
+from math import asin, sin
 from csv import reader
 
+# https://pypi.python.org/pypi/geographiclib
+from geographiclib.geodesic import Geodesic
+from geographiclib.constants import Constants
+
 def sharpshooter(points):
-	# Do the texas sharpshooter thing.
+	# TODO
+	
 	return []
+
+# http://www.movable-type.co.uk/scripts/latlong.html#cross-track
+def cross_track_distance(start, finish, target):
+
+	# Earth's mean radius.
+	R = Constants.WGS84_a
+
+	# Geodesic from start to target.
+	g = Geodesic.WGS84.Inverse(start[0], start[1], target[0], target[1])
+
+	# Geodesic from start to finish.
+	e = Geodesic.WGS84.Inverse(start[0], start[1], finish[0], finish[1])
+
+	d13 = g['s12']  # Distance from start to target.
+	b13 = g['azi1'] # Initial bearing from start to target.
+	b12 = e['azi1'] # Initial bearing from start to finish.
+
+	return abs(asin(sin(d13 / R) * sin(b13 - b12))) * R
+	
+	# TODO check against http://geo.javawa.nl/coordcalc/index_en.html calc #2
 
 with open('sources.json') as file:
 	sources = loads(file.read())
---
+++ test.py
@@ -0,0 +1,19 @@
+from math import asin, sin
+
+from geographiclib.geodesic import Geodesic
+from geographiclib.constants import Constants
+
+def cross_track_distance(start, finish, target):
+	R = Constants.WGS84_a
+	g = Geodesic.WGS84.Inverse(start[0], start[1], target[0], target[1])
+	e = Geodesic.WGS84.Inverse(start[0], start[1], finish[0], finish[1])
+	d13 = g['s12']
+	b13 = g['azi1']
+	b12 = e['azi1']
+	return abs(asin(sin(d13 / R) * sin(b13 - b12))) * R
+
+target = [52.20, 5.30]
+start  = [52.10, 5.50]
+finish = [52.26, 5.45]
+
+print cross_track_distance(start, finish, target)
